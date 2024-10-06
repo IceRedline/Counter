b62d89f427d8765743c1dd1cc958f79b42de358a
IceRedline <scpd2604@gmail.com>
2024-10-06 22:29:35 +0300

    Создан счётчик по усложненному заданию

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var changeHistoryView: UITextView!
+    var touchCount: UInt = 0
+    var changeHistory: String = "История изменений: \n"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeHistoryView.isEditable = false
     }
 
-
+    @IBAction func addButtonTouched() {
+        touchCount += 1
+        counterLabel.text = String(touchCount)
+        changeHistory += "\(Date().formatted(date: .numeric, time: .standard)): значение изменено на +1 \n"
+        changeHistoryView.text = changeHistory
+    }
+    
+    @IBAction func decreaseButtonTouched() {
+        if touchCount > 0 {
+            touchCount -= 1
+            counterLabel.text = String(touchCount)
+            changeHistory += "\(Date().formatted(date: .numeric, time: .standard)): значение изменено на -1 \n"
+            changeHistoryView.text = changeHistory
+        } else {
+            changeHistory += "\(Date().formatted(date: .numeric, time: .standard)): попытка уменьшить значение счётчика ниже 0 \n"
+            changeHistoryView.text = changeHistory
+        }
+    }
+    @IBAction func resetButtonTouched() {
+        touchCount = 0
+        counterLabel.text = String(touchCount)
+        changeHistory += "\(Date().formatted(date: .numeric, time: .standard)): значение сброшено \n"
+        changeHistoryView.text = changeHistory
+    }
+    
 }
 
